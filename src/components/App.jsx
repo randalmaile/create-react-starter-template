244af40f17f468744cc070e29b3cf208d1e57056
randy maile <randy.maile@sharp.com>
2021-03-07 16:00:01 -0800

    changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import logo from './logo.svg';
+import logo from '../assets/logo.svg';
 
 function App() {
   return (
---
+++ src/context/example/index.js
@@ -0,0 +1,2 @@
+// export { default } from './exampleContext';
+// export { default } from './exampleState';
